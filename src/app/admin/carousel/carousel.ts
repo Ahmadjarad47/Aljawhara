71fa55b04e1d15f556de4a9865034c8fc0a02029
ahmad222jarad <ahmadjarad@gmai.com>
2025-12-08 17:14:51 +0300

    Fix formatting in carousel component by removing unnecessary whitespace

--- src/app/admin/carousel/carousel.ts
+++ src/app/admin/carousel/carousel.ts
@@ -83,6 +83,7 @@ export class Carousel implements OnInit {
     this.carousels$.subscribe(items => length = items.length).unsubscribe();
     return length;
   }
+  
 
   // Forms
   newCarousel: CarouselCreateWithFileDto = {
