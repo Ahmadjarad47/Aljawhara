fb154d2fba7ea651a146594bb5c71c485773fa8e
ahmad222jarad <ahmadjarad@gmai.com>
2026-02-26 14:36:00 +0300

    Update API URL in development environment configuration to point to the production server, commenting out the local server URL for clarity and to prevent accidental usage.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // apiUrl: 'https://api.aljawharaplus.com/api/',
-    apiUrl: 'https://localhost:7130/api/',
+    apiUrl: 'https://api.aljawharaplus.com/api/',
+    // apiUrl: 'https://localhost:7130/api/',
 };  
